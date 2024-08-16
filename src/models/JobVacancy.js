c657c483f0cebff9ce358f40b79fff43cdecf190
Ath3Dev <arthur.aba20@gmail.com>
2024-08-16 16:51:49 -0300

    :star2: feat: JobVacancy Model

---
+++ src/models/JobVacancy.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const JobVacancySchema = mongoose.Schema(
+    {
+        companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+        jobTitle: { type: String, required: true },
+        jobDescription: { type: String, required: true },
+        salary: { type: String },
+        jobLocation: {
+            city: { type: String, required: true },
+            state: { type: String, required: true },
+        },
+        workSchedule: {
+            workingHours: { type: String },
+            workingDays: {
+                type: [String],
+                enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                default: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+            }
+        },
+        requiredQualifications: [{ type: String }],
+        desiredSkills: [{ type: String }],
+        employmentType: {
+            type: String,
+            enum: ['Full-time', 'Part-time', 'Contract', 'Temporary'],
+            required: true
+        },
+        applicationDeadline: { type: Date }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const JobVacancy = mongoose.model('JobVacancy', JobVacancySchema);
+
+module.exports = JobVacancy;
