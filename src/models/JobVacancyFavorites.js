16775874d0bacf6660f8650b0c3c5eec348bbefc
Arthur.exe <arthur.aba20@gmail.com>
2024-08-17 23:05:15 -0300

    :ship: feat: JobVacancyFavorites Model

---
+++ src/models/JobVacancyFavorites.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const JobVacancyFavoritesSchema = new mongoose.Schema(
+    {
+        companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+        jobVacancyId: { type: mongoose.Schema.Types.ObjectId, ref: 'JobVacancy', required: true },
+        favoriteCandidates: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Candidate', required: true }]
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Definindo um índice composto para garantir que não haja duplicação de vagas e empresas
+JobVacancyFavoritesSchema.index({ companyId: 1, jobVacancyId: 1 }, { unique: true });
+
+module.exports = mongoose.model('JobVacancyFavorites', JobVacancyFavoritesSchema);
