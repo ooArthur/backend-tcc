1488e35fd629ef8a61850912601e6b3d05db0042
Arthur.exe <arthur.aba20@gmail.com>
2024-09-06 23:05:06 -0300

    :books: feat: Inicio Documentação API

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./config/database");
 const router = require("./routes/router");
 const logger = require("./config/logger");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const yaml = require("js-yaml");
 
 const app = express();
 
@@ -28,7 +31,16 @@ app.use((req, res, next) => {
     next();
 });
 
+// Conectar ao banco de dados
 connectDB();
+
+// Carregar o arquivo swagger.yaml da pasta docs
+const swaggerDocument = yaml.load(fs.readFileSync('./src/docs/swagger.yaml', 'utf8'));
+
+// Middleware para servir a documentação Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Roteamento da API
 app.use("/api", router);
 
 const PORT = process.env.PORT || 5000;
@@ -36,6 +48,7 @@ app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
 });
 
+// Middleware de tratamento de erros
 app.use((err, req, res, next) => {
     logger.error(`Erro: ${err.message}`);
     res.status(500).json({ error: 'Ocorreu um erro interno.' });
